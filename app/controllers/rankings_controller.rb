0314dc135c1cb944d94e8d95372b232f1cdf8b04
Zuzanna Stolińska <zuzanna.st@gmail.com>
2015-03-27 22:57:50 +0100

    Awesome ranking upgraded.

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -2,7 +2,7 @@ class RankingsController < ApplicationController
 
   def index
     @top_players = Player.all.sort_by{ |player| player.win_rate }.reverse.first(3)
-    @top_day = Player.all.sort_by{ |player| player.won_today }.reverse
+    @top_day = Player.all.sort_by{ |player| player.win_rate_today }.reverse
     @top_active = Player.all.sort_by{ |player| player.matches_played }.reverse
   end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,26 +1,27 @@
 class Player < ActiveRecord::Base
-    
+
   has_many :player_matches
   has_many :matches, through: :player_matches
 
   validates :first_name, :last_name, presence: true, format: { with: /\A[a-zA-Z]+\z/,
     message: "only allows letters" }, length: { minimum: 2, message: "minimum 2 letters" }
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+    has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
 
   def full_name
     "#{first_name} #{last_name}"
   end
 
   def score(match)
-      score = player_matches.find_by(match_id: match.id).result
-    
-      if score.blank?
-        score = score.to_i
-      else
-        score
-      end
+    score = player_matches.find_by(match_id: match.id).result
+
+    if score.blank?
+      score = score.to_i
+    else
+      score
+    end
   end
 
   def goal!(match)
@@ -53,22 +54,73 @@ class Player < ActiveRecord::Base
     matches_won.count
   end
 
+  def goals_scored
+    scored = 0
+
+    matches.each do |match|
+      scored += score(match)
+    end
+    scored
+  end
+
+  def goals_lost
+    scored = 0
+    lost = 0
+
+    matches.each do |match|
+      match.players.each do |player|
+        if score(match) == player.score(match)
+          scored += score(match)
+        else
+          lost += player.score(match)
+        end
+      end
+    end
+    lost
+  end
+
   def win_rate
-    if matches_played == 0
-      0
+    if matches_played > 5
+      ((goals_scored.to_f - goals_lost) / matches_played).round(2)
     else
-      (matches_won.to_f / matches_played.to_f).round(2)
+      0
     end
   end
 
-  def won_today
-    won_today = []
+  def matches_played_today
+    matches.played_today.count
+  end
+
+  def goals_scored_today
+    scored_today = 0
 
     matches.played_today.each do |match|
-      if has_won?(match)
-      won_today << match
+      scored_today += score(match)
+    end
+    scored_today
+  end
+
+  def goals_lost_today
+    scored = 0
+    lost = 0
+
+    matches.played_today.each do |match|
+      match.players.each do |player|
+        if score(match) == player.score(match)
+          scored += score(match)
+        else
+          lost += player.score(match)
+        end
       end
     end
-    won_today.count
+    lost
+  end
+
+  def win_rate_today
+    if matches_played_today == 0
+      0
+    else
+      ((goals_scored_today.to_f - goals_lost_today) / matches_played_today).round(2)
+    end
   end
 end
