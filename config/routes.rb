99eb4f7ee6d1616b96c916d5b0ad472f5f9e9b9f
Zuzanna Stolińska <zuzanna.st@gmail.com>
2015-03-31 12:46:34 +0200

    Repaired routes for Match.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'ranking' => 'rankings#index'
   resources :players, except: [:destroy]
-  resources :matches, only: [:index, :show, :new] do 
+  resources :matches, except: [:edit, :destroy, :update] do 
     put 'first_player_goal', on: :member
     put 'second_player_goal', on: :member
   end
