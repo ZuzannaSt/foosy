6d76fc1c043669f913d97cd9d383ae83d7fbfbca
Zuzanna Stolińska <zuzanna.st@gmail.com>
2015-05-20 09:14:16 +0200

    Require Bootstrap JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require bootstrap
 //= require unobtrusive_flash
 //= require unobtrusive_flash_bootstrap
 //= require bootbox
