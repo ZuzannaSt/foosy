4b076074b6670f1c1a41db23f6998d53eddb5c2c
Zuzanna Stolińska <zuzanna.st@gmail.com>
2015-06-24 13:58:27 +0200

    separate link_to_fullname method

--- app/decorators/player_decorator.rb
+++ app/decorators/player_decorator.rb
@@ -3,6 +3,10 @@ class PlayerDecorator < ApplicationDecorator
   decorates_association :matches
 
   def full_name
+    "#{model.first_name} #{model.last_name}"
+  end
+
+  def link_to_full_name
     h.link_to "#{model.first_name} #{model.last_name}", model
   end
 
