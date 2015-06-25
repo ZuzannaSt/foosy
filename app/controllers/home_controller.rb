e869fa2dd172ccbcb43a637e192e2bcc34b02354
Zuzanna Stolińska <zuzanna.st@gmail.com>
2015-06-25 14:38:17 +0200

    add some cool charts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   def index
     @top_players = Player.all.decorate.sort_by{ |player| player.win_rate }.reverse.first(3)
-    @recent_games = Match.order("date DESC").decorate.first(8)
+    @recent_games = Match.order("date DESC").decorate.first(5)
+    @matches = Match.all
+
+    @matches_created = @matches.group("date(created_at)").count
   end
 end
--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -2,7 +2,11 @@ class RankingsController < ApplicationController
 
   def index
     @top_players = Player.all.decorate.sort_by{ |player| player.win_rate }.reverse.first(3)
-    @top_day = Player.all.decorate.sort_by{ |player| player.win_rate_today }.reverse
-    @top_active = Player.all.decorate.sort_by{ |player| player.matches_played }.reverse
+    @top_day = Player.all.decorate.sort_by{ |player| player.win_rate_today }.reverse.first(8)
+    @top_active = Player.all.decorate.sort_by{ |player| player.matches_played }.reverse.first(10)
+
+    @goals_scored = PlayerMatch.group(:result).count
+    @win_rate = Player.all.collect{|player| [player.first_name, player.win_rate.to_s]}
+    @matches_played = Player.all.collect{|player| [player.first_name, player.matches_played.to_s]}
   end
 end
